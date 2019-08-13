49a2f6b7ea515d7afb02cef2eed90c5ddcff1cdf
wahidmdn13 <wahidmadano@gmail.com>
2019-08-13 17:36:25 +0200

    Updates

---
+++ precache-manifest.c8b9bd44d79e3cd5be65f6162fa9c0e5.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "07b9975e68aa06401ea81cfdd8e6794f",
+    "url": "/EL-BESS/static/media/1.07b9975e.png"
+  },
+  {
+    "revision": "1eb6e537fbc42dd3eade",
+    "url": "/EL-BESS/static/css/main.5f93e978.chunk.css"
+  },
+  {
+    "revision": "f747b1305cc8a0db3afd",
+    "url": "/EL-BESS/static/js/runtime~main.f747b130.js"
+  },
+  {
+    "revision": "648ecf6387c97317ab11",
+    "url": "/EL-BESS/static/js/2.648ecf63.chunk.js"
+  },
+  {
+    "revision": "e70df97ec3f02c683f25e07b6b26df72",
+    "url": "/EL-BESS/static/media/wow.e70df97e.png"
+  },
+  {
+    "revision": "79c1998fb18c2d0078bee11f902364e3",
+    "url": "/EL-BESS/static/media/square-purple-1.79c1998f.png"
+  },
+  {
+    "revision": "338ae766238e6f49980d7044f01436fd",
+    "url": "/EL-BESS/static/media/2.338ae766.png"
+  },
+  {
+    "revision": "29c9bc0d3a6db748b9a00aadb47bd848",
+    "url": "/EL-BESS/static/media/7.29c9bc0d.png"
+  },
+  {
+    "revision": "f9913800b242016546a0b9b4bb912019",
+    "url": "/EL-BESS/static/media/9.f9913800.png"
+  },
+  {
+    "revision": "e37326c7e0fd9826c1a35b231340f58b",
+    "url": "/EL-BESS/static/media/path4.e37326c7.png"
+  },
+  {
+    "revision": "ceb241f05718fb1311f8810eea959803",
+    "url": "/EL-BESS/static/media/path5.ceb241f0.png"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/EL-BESS/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/EL-BESS/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/EL-BESS/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/EL-BESS/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/EL-BESS/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/EL-BESS/static/media/square5.ff004a0e.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/EL-BESS/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/EL-BESS/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/EL-BESS/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/EL-BESS/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "1eb6e537fbc42dd3eade",
+    "url": "/EL-BESS/static/js/main.1eb6e537.chunk.js"
+  },
+  {
+    "revision": "4454d9a3226629d5c958fc37c6858366",
+    "url": "/EL-BESS/static/media/3.4454d9a3.png"
+  },
+  {
+    "revision": "a49fb56393180715f9ecc6a34e34f996",
+    "url": "/EL-BESS/static/media/4.a49fb563.png"
+  },
+  {
+    "revision": "7a4c38fa92f939ef5ebe6897b51c9a41",
+    "url": "/EL-BESS/static/media/5.7a4c38fa.png"
+  },
+  {
+    "revision": "0e52cd506485979bc6525f225fabf533",
+    "url": "/EL-BESS/static/media/8.0e52cd50.png"
+  },
+  {
+    "revision": "068889ac359c1223148d18f2c93b0e3b",
+    "url": "/EL-BESS/static/media/10.068889ac.png"
+  },
+  {
+    "revision": "09fd27aa90ba849464cb8f2081ed3e5c",
+    "url": "/EL-BESS/static/media/11.09fd27aa.png"
+  },
+  {
+    "revision": "408cdf7528baf8b9b5977a4974a7f2d0",
+    "url": "/EL-BESS/static/media/12.408cdf75.png"
+  },
+  {
+    "revision": "cd740b770d4bd0d107af2c111855411f",
+    "url": "/EL-BESS/static/media/13.cd740b77.png"
+  },
+  {
+    "revision": "cc4bf88cb2b858cd4fdb245f950178c9",
+    "url": "/EL-BESS/static/media/14.cc4bf88c.png"
+  },
+  {
+    "revision": "afae74f641eeb8fd8effa4ec4d09f70d",
+    "url": "/EL-BESS/static/media/15.afae74f6.png"
+  },
+  {
+    "revision": "01899307116a937c66a0eb4b3927b631",
+    "url": "/EL-BESS/static/media/blob.01899307.png"
+  },
+  {
+    "revision": "8d581ffb03c56a0106abc6b330b449d5",
+    "url": "/EL-BESS/static/media/cercuri.8d581ffb.png"
+  },
+  {
+    "revision": "03ea2a0ad05689cebb5a9314a53b8966",
+    "url": "/EL-BESS/static/media/patrat.03ea2a0a.png"
+  },
+  {
+    "revision": "43ff45c5dc1f65726b86fc75ea6cabc0",
+    "url": "/EL-BESS/static/media/path2.43ff45c5.png"
+  },
+  {
+    "revision": "11fa5f73cb719f0f1a38e02e47bdc843",
+    "url": "/EL-BESS/static/media/waves.11fa5f73.png"
+  },
+  {
+    "revision": "a70167ea1491987c50049961a9e2e45f",
+    "url": "/EL-BESS/static/media/path3.a70167ea.png"
+  },
+  {
+    "revision": "d70c756f2a739722a398819cfa7287a7",
+    "url": "/EL-BESS/static/media/bitcoin.d70c756f.png"
+  },
+  {
+    "revision": "4fd59ead57f5b7dfa3ca0db01414a256",
+    "url": "/EL-BESS/static/media/etherum.4fd59ead.png"
+  },
+  {
+    "revision": "98742a820d2986e78ae284d57deae457",
+    "url": "/EL-BESS/static/media/ripp.98742a82.png"
+  },
+  {
+    "revision": "4002331ef45fb6171a60a219cfdcea29",
+    "url": "/EL-BESS/index.html"
+  }
+];
